ae7e411fa257095c5938a7440e4173428026b911
anasi <anass0202@gmail.com>
2026-06-07 03:51:24 +0530

    force cache clear

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'; // v4.16.19
 
 export default defineConfig({
   site: 'https://anasidris.co.in',
